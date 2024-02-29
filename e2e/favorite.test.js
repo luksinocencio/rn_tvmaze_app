4a4bbd5f4ef75cf86b37a4629be0bd5b786f2bfe
luksinocencio <luksinocencio@gmail.com>
2024-02-29 12:27:45 -0300

    *

--- e2e/favorite.test.js
+++ e2e/favorite.test.js
@@ -9,26 +9,26 @@ describe('Favorite', () => {
     await device.reloadReactNative()
   })
 
-  // it('Should favorite a TV show', async () => {
-  //   // 1. Digitar no campo de busca
-  //   const inputSearch = element(by.id('search-input'))
-  //   await inputSearch.replaceText('The Big Bang Theory')
-  //   await inputSearch.typeText('\n')
+  it('Should favorite a TV show', async () => {
+    // 1. Digitar no campo de busca
+    const inputSearch = element(by.id('search-input'))
+    await inputSearch.replaceText('The Big Bang Theory')
+    await inputSearch.typeText('\n')
 
-  //   // 2. Pressionar o card
-  //   const cardElement = element(by.text('The Big Bang Theory'))
-  //   await waitFor(cardElement).toBeVisible().withTimeout(5000)
-  //   await cardElement.atIndex(1).tap()
+    // 2. Pressionar o card
+    const cardElement = element(by.text('The Big Bang Theory'))
+    await waitFor(cardElement).toBeVisible().withTimeout(5000)
+    await cardElement.atIndex(1).tap()
 
-  //   // 3. Pressionar o botão favorito
-  //   const buttonElement = element(by.id('favorite-button'))
-  //   await waitFor(buttonElement).toBeVisible().withTimeout(5000)
-  //   await buttonElement.tap()
+    // 3. Pressionar o botão favorito
+    const buttonElement = element(by.id('favorite-button'))
+    await waitFor(buttonElement).toBeVisible().withTimeout(5000)
+    await buttonElement.tap()
 
-  //   // 4. VERIFICAR se o coração ficou vermelho
-  //   const heartIconElement = element(by.id('heart-icon-true'))
-  //   await expect(heartIconElement).toBeVisible()
-  // })
+    // 4. VERIFICAR se o coração ficou vermelho
+    const heartIconElement = element(by.id('heart-icon-true'))
+    await expect(heartIconElement).toBeVisible()
+  })
 
   it('Should favorite the first bad TV Show (star < 5)', async () => {
     // 1.
